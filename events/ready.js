1b61dd7c21ea8a71b0a74129abd9af3aa428e736
gitflee <fleeprivate@gmail.com>
2019-12-29 16:02:44 +0100

    Bot State updates

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,4 @@
 module.exports = (client) => {
+  client.user.setActivity(`${client.config.version}`, {type: "PLAYING"});
   console.log(`Bot active in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
   }
