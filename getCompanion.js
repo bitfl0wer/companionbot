419fbbd6180fb1b54a6f13282426b38d79ef8fba
gitflee <fleeprivate@gmail.com>
2019-12-29 16:49:03 +0100

    temp fix

--- getCompanion.js
+++ getCompanion.js
@@ -1,10 +1,18 @@
 exports.getCompanion = function (rarity, client) {
+    let nocompanion = 0;
+    let ran;
     let promise = false;
     while (promise === false) {
-        let ran = client.companions.randomKey(1);
+        if(nocompanion === 10000) {
+            ran = 'Senko'; //THIS IS A TEMPORARY FIX
+            promise = true;
+            return ran;
+        }
+        ran = client.companions.randomKey(1);
         if(client.companions.get(ran, 'rarity') === rarity){
             promise = true;
             return ran;
         }
+        nocompanion++
     }
 };
