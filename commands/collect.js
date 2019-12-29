9529c7c86e4d131201e7fc9754c4e9837386f4f2
gitflee <fleeprivate@gmail.com>
2019-12-29 16:18:06 +0100

    FIX collect.js - Companions can't be collected infinitely anymore

--- commands/collect.js
+++ commands/collect.js
@@ -2,14 +2,20 @@ exports.run = (client, message, args) => {
     //Fix: You can Collect infinite of the same companion
     let authorID = message.author.id;
     arg = String(args[0]).toLowerCase();
+    console.log('Arg: ' + arg);
     scomp = String(client.serverdata.get(message.guild.id, 'scompanion')).toLowerCase();
+    console.log('Scomp: ' + scomp);
+    let ucomps = Array.from(client.userdata.get(authorID, 'companions'));
+    let argButCapital = arg.charAt(0).toUpperCase() + arg.slice(1);
+    let hasComp = ucomps.includes(argButCapital);
+    console.log('has? ' + hasComp);
     if (scomp === 'none') {
         return message.channel.send(":exclamation: This server either doesn't use the Collect-Spawnsystem, the spawnchannel has not been set up right, or there are no uncollected companions. ||Tell an Admin to set a spawnchannel with `%settings setchannel`, if they haven't already!||");
     } else if (scomp === 'collected') {
         return message.channel.send(':grey_exclamation: There is no companion to collect.');
     } else {
         if (arg === scomp) {
-            if (String(client.userdata.get("companions")).indexOf(`${client.serverdata.get(message.guild.id, 'scompanion')}`) !== -1) {
+            if (hasComp === true) {
                 return message.channel.send(':grey_exclamation: You already own this companion. Let someone else collect it instead! :smile:');
             } else {
                 message.channel.send(`:white_check_mark: You have successfully collected ${client.serverdata.get(message.guild.id, 'scompanion')}! Select your new companion with %select ${client.serverdata.get(message.guild.id, 'scompanion')}`);
